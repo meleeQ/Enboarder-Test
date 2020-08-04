bea79792029447c5f933d2c1146c89cbdabbfe84
Jacklin <wlgqimingxue2016@gmail.com>
2020-08-05 08:55:39 +1000

    modify-db-table-spaceData

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,10 @@
 CREATE TABLE spaceData (
   id varchar(255) NOT NULL,
-  spaceItem varchar(8000) NOT NULL,
+  full_name varchar(255) NOT NULL,
+  stat varchar (255) NOT NULL,
+  location_name varchar(255),
+  location_region varchar(255),
+  location_longitude decimal(15),
+  location_latitude decimal(15),
   PRIMARY KEY (id)
 );
