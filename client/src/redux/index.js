8a24cc25e8ca65655b5e1ed1a3258bf6cbb353fd
Jacklin <wlgqimingxue2016@gmail.com>
2020-08-05 08:54:56 +1000

    add-redux-reducer-logic

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,14 +1,39 @@
 import { createStore, combineReducers, compose } from 'redux';
+import {
+    GET_LANDINGPAD,
+    GET_CAPSULES,
+    GET_ERRORS
+} from '../constants'
 
 const { NODE_ENV } = process.env;
 const isDevelopment = NODE_ENV === 'development';
-
+const initialState = {
+        capsules:[],
+        landingPad: null,
+        error:null
+    }
+    
 const reducers = {
-    spaceData: (oldState = {}, action) => {
+    spaceData: (state = initialState, action) => {
         const { type } = action;
         switch (type) {
+            case GET_LANDINGPAD:
+            return {
+                ...state,
+                landingPad: action.payload
+            }
+            case GET_CAPSULES:
+            return {
+                ...state,
+                capsules: action.payload
+            }
+            case GET_ERRORS:
+            return {
+                ...state,
+                error: action.payload
+            }
             default:
-                return oldState;
+                return state;
         }
     },
 };
