c35cabba7e80cb46ddb943d5a6b9878977ebf7fd
Jacklin <wlgqimingxue2016@gmail.com>
2020-08-05 08:50:06 +1000

    modify-index-App

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,14 +1,33 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import GlobalStyle from '../theme';
-import { Application } from './styles';
-import { ReactComponent as Rocket } from '../assets/rocket.svg';
+import styled from 'styled-components';
+import { ControlConsole } from '../components/Control/ControlConsole';
+import  DisplyConsole  from '../components/Disply/DisplyConsole';
+
+const Application = styled.div`
+    font-family: Roboto;
+    font-weight: 300;
+    font-style: italic;
+    color: black;
+    margin:0 auto;
+    paddding:0;
+    width:50%;
+    height:100%;
+    position:abosulte;
+    display:block;
+    @media(max-width:768px){
+        display:flex;
+        margin:0;  
+        width:100%;     
+    }
+`;
 
 const App = () => (
     <>
         <Application >
-            <Rocket />
-            <span>"Space isn't remote at all. It's only an hour's drive away, if your car could go straight upwards."</span>
+        <DisplyConsole />
+        <ControlConsole />    
         </Application>
         <GlobalStyle />
     </>
